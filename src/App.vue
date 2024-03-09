3ecc215b7cfcbeb3ebda6049c2b3c35b2bec1879
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-03-09 14:24:15 +0300

    add component Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
 </script>
 
 <template>
-  
+  <Header />
 </template>
 
-<style>
-</style>
+<style></style>
