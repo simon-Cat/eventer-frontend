ad4064e22cc56e35433066de755a48f748c4a701
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-03-24 17:48:45 +0300

    add adaptive for Features

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -12,15 +12,9 @@ export default {}
           Организовывайте вечеринки, концерты, мастер-классы, выставки, семинары и другие
           мероприятия, которые будут интересны и полезны для участников.
         </p>
-        <img
-          class="list-item-img"
-          src="../assets/images/landing-img-1.png"
-          alt=""
-          width="424"
-          height="270"
-        />
+        <img class="list-item-img" src="../assets/images/landing-img-1.png" alt="" />
       </li>
-      <li class="list-item">
+      <li class="list-item" id="list-item-revert">
         <div class="vector features-vector" id="features-vector-2"></div>
         <img
           class="list-item-img"
@@ -37,8 +31,8 @@ export default {}
       <li class="list-item">
         <div class="vector features-vector" id="features-vector-3"></div>
         <p class="list-item-text">
-          Держите подписчиков в курсе о любых актуализациях и событиях,
-          происходящих в рамках вашего мероприятия.
+          Держите подписчиков в курсе о любых актуализациях и событиях, происходящих в рамках вашего
+          мероприятия.
         </p>
         <img
           class="list-item-img"
@@ -54,17 +48,17 @@ export default {}
 
 <style scoped>
 .eventer-features {
-  padding-bottom: 100px;
+  padding-bottom: 350px;
   position: relative;
 }
 
 .title {
   max-width: 380px;
-  margin: 90px auto 0;
+  margin: 190px auto 80px;
   font-size: var(--text-size-40);
   z-index: 1;
   position: relative;
-  margin-bottom: 80px;
+  text-align: center;
 }
 
 .list-item {
@@ -72,6 +66,7 @@ export default {}
   justify-content: space-around;
   margin-bottom: 100px;
   position: relative;
+  background-color: var(--white-color);
 }
 
 .features-vector {
@@ -79,7 +74,6 @@ export default {}
   height: 350px;
   background: url(../assets/images/vectors/features-vector.svg) no-repeat;
   background-size: contain;
-  z-index: -1;
 }
 
 #features-vector-1 {
@@ -109,10 +103,12 @@ export default {}
   line-height: 30px;
   align-self: center;
   text-align: center;
+  z-index: 1;
 }
 
 .list-item-img {
-  width: 600px;
+  width: 100%;
+  max-width: 600px;
   height: 500px;
   object-fit: contain;
   border-radius: var(--raduis-10);
@@ -121,5 +117,92 @@ export default {}
 
 .text-blue {
   color: var(--blue-color);
+}
+
+@media screen and (max-width: 1309px) {
+  .eventer-features {
+    padding-bottom: 300px;
+  }
+
+  .list-item {
+    flex-direction: column;
+    align-items: center;
+    row-gap: 40px;
+    margin-bottom: 150px;
+  }
+
+  #list-item-revert {
+    flex-direction: column-reverse;
+  }
+
+  .list-item-text {
+    font-size: var(--text-size-24);
+  }
+
+  .features-vector {
+    visibility: hidden;
+  }
+}
+
+@media screen and (max-width: 685px) {
+  .list-item-text {
+    max-width: 450px;
+  }
+
+  .list-item-img {
+    max-width: 500px;
+  }
+
+  .title {
+    margin-bottom: 60px;
+    margin-top: 160px;
+  }
+
+  .list-item {
+    margin-bottom: 120px;
+  }
+}
+
+@media screen and (max-width: 581px) {
+  .title {
+    font-size: var(--text-size-35);
+    margin-top: 140px;
+  }
+
+  .list-item-text {
+    font-size: var(--text-size-20);
+    max-width: 350px;
+  }
+
+  .list-item-img {
+    max-width: 450px;
+    height: 350px;
+  }
+}
+
+@media screen and (max-width: 445px) {
+  .eventer-features {
+    padding-bottom: 250px;
+    position: relative;
+  }
+
+  .title {
+    font-size: var(--text-size-30);
+    margin-top: 120px;
+    margin-bottom: 50px;
+  }
+
+  .list-item-text {
+    font-size: var(--text-size-18);
+  }
+
+  .list-item-img {
+    max-width: 400px;
+    height: 300px;
+  }
+
+  .list-item {
+    margin-bottom: 90px;
+  }
 }
 </style>
