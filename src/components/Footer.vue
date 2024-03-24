73466827c5d06e601e5b068a08585e1d49acd17c
Bekir Murtazaev <bekirmurtazaev@yandex.ru>
2024-03-24 22:08:40 +0300

    add adaptive for Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@ export default {}
 <template>
   <footer class="footer">
     <div class="vector" id="footer-vector"></div>
-    <p class="footer__text">Давайте начнем объединять людей.</p>
+    <p class="footer__text">Объединим людей вместе!</p>
   </footer>
 </template>
 
@@ -23,10 +23,58 @@ export default {}
 
 #footer-vector {
   background: url(../assets/images/vectors/footer-vector.svg) no-repeat;
-  background-size: cover;
+  background-size: contain;
   width: 100%;
-  height: 200px;
+  height: 100%;
   top: -163px;
   z-index: -1;
+}
+
+@media screen and (max-width: 1101px) {
+  #footer-vector {
+    top: -120px;
+  }
+}
+
+@media screen and (max-width: 810px) {
+  #footer-vector {
+    top: -100px;
+  }
+}
+
+@media screen and (max-width: 674px) {
+  #footer-vector {
+    top: -80px;
+  }
+}
+
+@media screen and (max-width: 539px) {
+  #footer-vector {
+    top: -60px;
+  }
+}
+
+@media screen and (max-width: 403px) {
+  #footer-vector {
+    top: -40px;
+  }
+}
+
+@media screen and (max-width: 685px) {
+  .footer__text {
+    font-size: var(--text-size-35);
+  }
+}
+
+@media screen and (max-width: 685px) {
+  .footer__text {
+    font-size: var(--text-size-30);
+  }
+}
+
+@media screen and (max-width: 445px) {
+  .footer__text {
+    font-size: var(--text-size-26);
+  }
 }
 </style>
